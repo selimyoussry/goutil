3b1d3e97e50c14488e987836030b3c1a28bf7a04
selim <selim@wearkinetic.com>
2017-04-22 09:46:22 -0400

    better url helper

--- url.go
+++ url.go
@@ -1,9 +1,26 @@
 package goutil
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
+
+// URL wraps a url string with its GET parameters
+type URL struct {
+	URL           string
+	GetParameters map[string]interface{}
+}
+
+// NewURL instanciates
+func NewURL(url string) *URL {
+	return &URL{
+		URL:           url,
+		GetParameters: map[string]interface{}{},
+	}
+}
 
 // URLJoin takes a bunch of urls and concatenate them
-func URLJoin(ss ...string) string {
+func URLJoin(ss ...string) *URL {
 	parts := []string{}
 
 	for _, s := range ss {
@@ -17,9 +34,47 @@ func URLJoin(ss ...string) string {
 		parts = append(parts, urlJoinRemoveExtremeSlashes(s))
 	}
 
-	return strings.Join(parts, "/")
+	return NewURL(strings.Join(parts, "/"))
 }
 
+// SetURLParameter sets one parameters
+func (u *URL) SetURLParameter(key string, value interface{}) *URL {
+	u.GetParameters[key] = value
+	return u
+}
+
+// SetURLParameters sets one parameters
+func (u *URL) SetURLParameters(params map[string]interface{}) *URL {
+	for key, value := range params {
+		u.GetParameters[key] = value
+	}
+	return u
+}
+
+// Format creates the complete URL
+func (u *URL) Format() string {
+	if len(u.GetParameters) == 0 {
+		return u.URL
+	}
+
+	kv := []string{}
+	for k, v := range u.GetParameters {
+		vStr := fmt.Sprintf("%v", v)
+		var s string
+		if (vStr == "") || (v == nil) {
+			s = k
+		} else {
+			s = fmt.Sprintf("%s=%s", k, vStr)
+		}
+
+		kv = append(kv, s)
+	}
+
+	return fmt.Sprintf("%s?%s", u.URL, strings.Join(kv, "&"))
+}
+
+// urlJoinRemoveExtremeSlashes removes first and last slash of a URL part
+// useful for joining chunks to make a complete URL
 func urlJoinRemoveExtremeSlashes(s string) string {
 	if s == "" {
 		return s
--- url_test.go
+++ url_test.go
@@ -5,9 +5,9 @@ import "testing"
 func TestURL(t *testing.T) {
 
 	urls := []string{}
-	urls = append(urls, URLJoin("http://google.com/", "a", "b/", "c/d/e/", "f/g"))
-	urls = append(urls, URLJoin("http://google.com", "a", "b/", "/c/d/e/", "f/g"))
-	urls = append(urls, URLJoin("http://google.com", "a", "", "b/", "", "c/d/e/", "/f/g/"))
+	urls = append(urls, URLJoin("http://google.com/", "a", "b/", "c/d/e/", "f/g").Format())
+	urls = append(urls, URLJoin("http://google.com", "a", "b/", "/c/d/e/", "f/g").Format())
+	urls = append(urls, URLJoin("http://google.com", "a", "", "b/", "", "c/d/e/", "/f/g/").Format())
 
 	expectedURL := "http://google.com/a/b/c/d/e/f/g"
 	for i, url := range urls {
@@ -15,4 +15,27 @@ func TestURL(t *testing.T) {
 			t.Errorf("Expected %s - Got %s - For #%d", expectedURL, url, i)
 		}
 	}
+
+	urlWithParameter := URLJoin("http://google.com/", "a", "b/", "c/d/e/", "f/g").
+		SetURLParameter("hello", "world").
+		SetURLParameter("k", nil).
+		SetURLParameter("number", 10.2)
+
+	expectedURLs := []string{
+		"http://google.com/a/b/c/d/e/f/g?hello=world&number=10.2&k",
+		"http://google.com/a/b/c/d/e/f/g?hello=world&k&number=10.2",
+		"http://google.com/a/b/c/d/e/f/g?k&number=10.2&hello=world",
+		"http://google.com/a/b/c/d/e/f/g?k&hello=world&number=10.2",
+		"http://google.com/a/b/c/d/e/f/g?number=10.2&k&hello=world",
+		"http://google.com/a/b/c/d/e/f/g?number=10.2&hello=world&k",
+	}
+
+	if !IsIn(urlWithParameter.Format(), expectedURLs...) {
+		t.Errorf("%s - Expected sth like %s - %s",
+			"URL Parameter parsing",
+			expectedURLs[0],
+			urlWithParameter.Format(),
+		)
+	}
+
 }
