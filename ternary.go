7b6cf2440c38c1d3a7090904e4b16da71e89d36d
selim <selim@wearkinetic.com>
2017-08-15 16:19:36 -0400

    ternary

--- ternary.go
+++ ternary.go
@@ -1,7 +1,27 @@
 package goutil
 
+import "time"
+
 // Implements cond ? _true : _false
 func TerString(cond bool, _true, _false string) string {
+	if cond {
+		return _true
+	}
+
+	return _false
+}
+
+// Implements cond ? _true : _false
+func TerTime(cond bool, _true, _false time.Time) time.Time {
+	if cond {
+		return _true
+	}
+
+	return _false
+}
+
+// Implements cond ? _true : _false
+func TerInt(cond bool, _true, _false int) int {
 	if cond {
 		return _true
 	}
